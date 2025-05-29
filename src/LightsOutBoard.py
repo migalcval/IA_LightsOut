e5a0ee31a15acbdb7f943a3fb255a0f224ab3379
migalcval <migalcval@alum.us.es>
2025-05-29 20:45:43 +0200

    avance tablero v0.1

---
+++ src/LightsOutBoard.py
@@ -0,0 +1,40 @@
+import random
+
+class LightsOutBoard:
+    
+    def __init__(self, size=5, randomize=False):
+        self.size = size
+        if randomize:
+            self.board = [[random.choice([0, 1]) for _ in range(size)] for _ in range(size)]
+        else:
+            self.board = [[0 for _ in range(size)] for _ in range(size)]
+
+    def toggle(self, i, j):
+        if 0 <= i < self.size and 0 <= j < self.size:
+            self.board[i][j] = 1 - self.board[i][j]
+
+    def press(self, i, j):
+        for x, y in [(0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)]:
+            self.toggle(i + x, j + y)
+
+    def win(self):
+        return all(cell == 1 for row in self.board for cell in row)
+
+    def print_board(self):
+        for row in self.board:
+            print(" ".join(str(cell) for cell in row))
+        print()
+
+#Test
+if __name__ == "__main__":
+    
+    board = LightsOutBoard(size=5, randomize=False)
+    print("Tablero inicial:")
+    board.print_board()
+
+    board.press(2, 2)
+    board.print_board()
+    board.press(2, 4)
+    board.print_board()
+
+    print("¿Tablero resuelto?:", board.win())
